882c0ac17c166bde8fbede6f0383a16a86c8201d
Anand Tripathi <anand.tripathi507@gmail.com>
2022-10-31 18:43:43 +0100

    migrate, upgrade downgrade added

--- commands.py
+++ commands.py
@@ -1,5 +1,4 @@
 import os
-from configparser import ConfigParser
 
 import typer
 from rich import print
@@ -8,7 +7,6 @@ from config import Config
 from constants import CONFIG_INI
 from es_wrapper import ESWrapper
 from migrate import Migrate
-from utils import exceptions
 
 app = typer.Typer(rich_markup_mode="rich")
 config = Config()
@@ -46,6 +44,27 @@ def init() -> None:
 def revision(message: str = typer.Option(..., "--message", "-m",
                                          help="message will be part of the migration file name")):
     migrate.create_revision(message=message)
+
+
+@app.command(
+    help="Command to upgrade [green bold]migration version[/green bold]")
+def upgrade(version: str = typer.Option("head",
+                                        help="If not version provided then upgrade to latest version else upgrade to specific version",
+                                        )):
+    migrate.upgrade(version)
+    print("Chalan [green bold]successful[/green bold].")
+
+
+@app.command(
+    help="Command to downgrade [green bold]migration version[/green bold]")
+def downgrade(version: str = typer.Option(None,
+                                          help="downgrade to specific version. Specify 'base' to migrate to initial version",
+                                          ), level: int = typer.Option(1,
+                                                                       help="downgrade number of level down")):
+    if version == 'base':
+        level = -1
+    migrate.downgrade(version, level)
+    print("Chalan [green bold]successful[/green bold].")
 
 
 if __name__ == '__main__':
--- migrate.py
+++ migrate.py
@@ -1,7 +1,8 @@
 import importlib
 import os
 from datetime import datetime
-
+from pydoc import importfile
+from rich import print
 from mako.template import Template
 
 from constants import VERSIONS_DIR
@@ -39,7 +40,54 @@ class Migrate:
             down_revision=version,
             revision=revision
         )
-        with open(
-                self.config.versions + f'/{revision}_{"_".join(message.split(" "))}.py',
-                'w') as f:
+        file_name = self.config.versions + f'/{revision}_{"_".join(message.split(" "))}.py'
+        with open(file_name, 'w') as f:
             f.write(content)
+            print(
+                f"Migration file [green bold]{file_name}[/green bold] created successfully")
+
+    def upgrade(self, version):
+        self._index_exists()
+        current_version = self.es.current_version()
+        for file_name in os.listdir(self.config.versions):
+            if '.py' not in file_name:
+                continue
+            file = importfile(self.config.versions + '/' + file_name)
+            if hasattr(file,
+                       'down_revision') and file.down_revision == current_version:
+                file.upgrade(self.es.client)
+                self.es.client.index(id='1',
+                                     index=self.config.es['migration_index'],
+                                     document=dict(version=file.revision),
+                                     refresh=True)
+                print(
+                    f"********* {file.down_revision or 'HEAD'} ---> {file_name} *********")
+                if version and version == file.revision:
+                    break
+                self.upgrade(version)
+                break
+
+    def downgrade(self, version, level=1):
+        if version is None and level == 0:
+            return
+        self._index_exists()
+        current_version = self.es.current_version()
+
+        for file_name in os.listdir(self.config.versions):
+            if '.py' not in file_name:
+                continue
+            file = importfile(self.config.versions + '/' + file_name)
+            if hasattr(file,
+                       'revision') and file.revision == current_version:
+                file.downgrade(self.es.client)
+                self.es.client.index(id='1',
+                                     index=self.config.es['migration_index'],
+                                     document=dict(version=file.down_revision),
+                                     refresh=True)
+                print(
+                    f"********* {file_name}  ---> {file.down_revision or 'HEAD'} *********")
+                if version and version == file.revision:
+                    break
+                if level > 0:
+                    level -= 1
+                self.downgrade(version, level)
